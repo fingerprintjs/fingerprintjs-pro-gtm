090daddcd007881577ce4bbaf7c26aa8388f7c91
Ilya Taratukhin <darkilfa@gmail.com>
2022-03-29 16:50:57 +0300

    chore: remove iife format

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from '@rollup/plugin-typescript'
 import jsonPlugin from '@rollup/plugin-json'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-const terserPlugin = require('rollup-plugin-terser').terser
 import licensePlugin from 'rollup-plugin-license'
 import { join } from 'path'
 
@@ -27,28 +26,7 @@ const commonOutput = {
   exports: 'named',
 }
 
-// Need for IIFE build
-const commonTerser = terserPlugin(require('./terser.config.js'))
-
 export default [
-  {
-    ...commonInput,
-    output: [
-      // IIFE build for browser with adding globals to window
-      {
-        ...commonOutput,
-        file: `${outputDirectory}/${artifactName}.min.js`,
-        format: 'iife',
-        plugins: [commonTerser],
-      },
-      {
-        ...commonOutput,
-        file: `${outputDirectory}/${artifactName}.js`,
-        format: 'iife',
-      },
-    ],
-  },
-
   // NPM bundles. They have all the dependencies excluded for end code size optimization.
   {
     ...commonInput,
