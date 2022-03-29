7c8eaa0c2fb3119e552022f0347e15f7dccf6000
Ilya Taratukhin <darkilfa@gmail.com>
2022-03-29 14:19:01 +0300

    feat: use separate options for load and get

--- src/load.ts
+++ src/load.ts
@@ -1,9 +1,12 @@
 import * as FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
 import type { LoadOptions, GetOptions, GetResult } from '@fingerprintjs/fingerprintjs-pro'
-import { pick } from './pick'
 
-export const load = (options: LoadOptions & GetOptions<boolean>, callback: (result: GetResult) => void) => {
-  const fpPromise = FingerprintJS.load(pick(options, ['region', 'endpoint']) as LoadOptions)
+export const load = (
+  loadOptions: LoadOptions,
+  getOptions: GetOptions<boolean>,
+  callback: (result: GetResult) => void
+) => {
+  const fpPromise = FingerprintJS.load(loadOptions)
 
-  fpPromise.then((fp) => fp.get(pick(options, ['tag', 'linkedId']))).then((result) => callback(result))
+  fpPromise.then((fp) => fp.get(getOptions)).then((result) => callback(result))
 }
