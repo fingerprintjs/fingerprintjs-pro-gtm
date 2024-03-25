98fce81fd3eafaed8f861c0d9513eb0119f22576
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-03-25 13:07:02 +0900

    build: add script for updating metadata

---
+++ scripts/updateMetadata.ts
@@ -0,0 +1,33 @@
+import * as yaml from 'yaml'
+import * as path from 'path'
+import * as fs from 'fs'
+
+type Metadata = {
+  versions: { sha: string; changeNotes: string }[]
+}
+
+async function main() {
+  const metadataPath = path.resolve(__dirname, '../metadata.yaml')
+  const metadata = yaml.parse(fs.readFileSync(metadataPath).toString()) as Metadata
+
+  const args = process.argv.slice(2)
+
+  if (args.length !== 2) {
+    throw new Error('Usage: updateMetadata <sha> <changeNotes>')
+  }
+
+  const [sha, changeNotes] = args
+
+  if (!sha || !changeNotes) {
+    throw new Error('Missing required args')
+  }
+
+  metadata.versions.push({ sha, changeNotes })
+
+  fs.writeFileSync(metadataPath, yaml.stringify(metadata))
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
